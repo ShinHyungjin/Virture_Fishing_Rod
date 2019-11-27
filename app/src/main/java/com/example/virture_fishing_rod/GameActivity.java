af5c7bfb624c3ec269d8cf1a21185da2437a2436
ntayuko <ntayuko@gmail.com>
2019-11-27 16:03:36 +0900

    낚싯대, DB 추가

--- app/src/main/java/com/example/virture_fishing_rod/GameActivity.java
+++ app/src/main/java/com/example/virture_fishing_rod/GameActivity.java
@@ -2,32 +2,119 @@ package com.example.virture_fishing_rod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements SensorEventListener {
     MediaPlayer player;
     Intent intent;
+    SensorManager m; Sensor sen; ImageView rod1,rod2,rod3;
+    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
         player = MediaPlayer.create(this,R.raw.splash02);
         player.setLooping(true);
         player.start();
 
         ImageView s = findViewById(R.id.iv2);
+        rod1 = findViewById(R.id.iv3);
+        rod2 = findViewById(R.id.iv4);
+        rod3 = findViewById(R.id.iv5);
+
         GlideDrawableImageViewTarget gif = new GlideDrawableImageViewTarget(s);
         Glide.with(this).load(R.drawable.ocean).into(gif);
+        GlideDrawableImageViewTarget png1 = new GlideDrawableImageViewTarget(rod1);
+        Glide.with(this).load(R.drawable.exrod).into(png1);
+        GlideDrawableImageViewTarget png2 = new GlideDrawableImageViewTarget(rod2);
+        Glide.with(this).load(R.drawable.exrod).into(png2);
+        GlideDrawableImageViewTarget png3 = new GlideDrawableImageViewTarget(rod3);
+        Glide.with(this).load(R.drawable.exrod).into(png3);
+
+        rod2.setVisibility(View.INVISIBLE);
+        rod3.setVisibility(View.INVISIBLE);
+
+        m = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sen = m.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        if(s==null){
+            Toast.makeText(this,"방향센서 없음->프로그램 종료",Toast.LENGTH_LONG).show();
+            finish();
+        }
+        m.registerListener(this,sen,SensorManager.SENSOR_DELAY_UI);
+
+        dbHelper helper = new dbHelper(this);
+        if(db == null) {
+            db = helper.getWritableDatabase();
+            db.execSQL("INSERT INTO 물고기 VALUES ('" + "가다랑어" + "','" + "고등어과의 어류 가운데 소형 종에 속한다. 몸은 굵고 통통한 방추형임" +
+                    " 등은 청흑색, 배는 광택을 띤 은백색이고 그 위에 4~10줄의 검은 세로띠가 있는 것이 특징이다." + "')");
+            db.execSQL("INSERT INTO 물고기 VALUES ('" + "갈치" + "','" + "광택이 나는 은백색을 띠며 등지느러미는 연한 황록색을 띤다." +
+                    " 눈이 머리에 비해 큰 편이며, 입 또한 크다. " + "칼치 또는 도어 라고도 한다" + "')");
+            db.execSQL("INSERT INTO 물고기 VALUES ('" + "감성돔" + "','" + "지느러미가 크게 발달되어 있으먀, 갑각류나 패류 등 작은 수생물을 포식한다." +
+                    " 참돔에 비하면 성장속도가 느리며, 우리나라 근해, 일본의 북해 등 널리 분포하며 서식한다." + "')");
+            db.execSQL("INSERT INTO 물고기 VALUES ('" + "고등어" + "','" + "가을철에 맛이 제일 좋으며, 연해에 분포되어 비교적 많이 잡히는 생선이다." +
+                    " 오메가-3 지방산이 많은 생선으로 유명하다." + "')");
+            db.execSQL("INSERT INTO 물고기 VALUES ('" + "대구" + "','" + "먹성이 대단한 포식성 어류로서, 입과 머리가 크다 해서 ㅁㅁ로 불리우는 한류성 어종이다." +
+                    "뒷지느러미는 두 개로 검고, 등지느러미는 세 개로 넓게 퍼져 있으며 가슴지느러미와 함께 노란색을 띤다." + "')");
+        }
+    }
+
+    class dbHelper extends SQLiteOpenHelper {
+        public dbHelper(Context context) {
+            super(context, "fish.db", null, 1);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE 물고기 (이름 TEXT, 정보 TEXT);");
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS 물고기");
+            onCreate(db);
+        }
     }
     public void onBackPressed(){
         player.stop();
         intent = new Intent(GameActivity.this, LoadingActivity.class);
         startActivity(intent);
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ORIENTATION){
+            float roll = event.values[2];
+            if(roll >= 20) {
+                rod2.setVisibility(View.VISIBLE);
+                rod1.setVisibility(View.INVISIBLE);
+                rod3.setVisibility(View.INVISIBLE);
+            }else if(roll<-20){
+                rod3.setVisibility(View.VISIBLE);
+                rod1.setVisibility(View.INVISIBLE);
+                rod2.setVisibility(View.INVISIBLE);
+            }else{
+                rod1.setVisibility(View.VISIBLE);
+                rod2.setVisibility(View.INVISIBLE);
+                rod3.setVisibility(View.INVISIBLE);
+            }
+            rod1.invalidate();
+            rod2.invalidate();
+            rod3.invalidate();
+        }
     }
 }

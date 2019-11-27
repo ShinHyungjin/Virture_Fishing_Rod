b5da1ad9845e3c6d596789efe5123ffaf8a50768
ntayuko <ntayuko@gmail.com>
2019-11-27 13:40:41 +0900

    2번째 페이지 배경 추가

--- app/src/main/java/com/example/virture_fishing_rod/GameActivity.java
+++ app/src/main/java/com/example/virture_fishing_rod/GameActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class GameActivity extends AppCompatActivity {
     MediaPlayer player;
@@ -16,6 +20,10 @@ public class GameActivity extends AppCompatActivity {
         player = MediaPlayer.create(this,R.raw.splash02);
         player.setLooping(true);
         player.start();
+
+        ImageView s = findViewById(R.id.iv2);
+        GlideDrawableImageViewTarget gif = new GlideDrawableImageViewTarget(s);
+        Glide.with(this).load(R.drawable.ocean).into(gif);
     }
     public void onBackPressed(){
         player.stop();

a33368666313a508e0de323edfebc33e68a2e499
ntayuko <ntayuko@gmail.com>
2019-11-27 13:25:04 +0900

    뒤로가기 음악 정지 재생 추가

--- app/src/main/java/com/example/virture_fishing_rod/GameActivity.java
+++ app/src/main/java/com/example/virture_fishing_rod/GameActivity.java
@@ -2,11 +2,13 @@ package com.example.virture_fishing_rod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
     MediaPlayer player;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +16,10 @@ public class GameActivity extends AppCompatActivity {
         player = MediaPlayer.create(this,R.raw.splash02);
         player.setLooping(true);
         player.start();
+    }
+    public void onBackPressed(){
+        player.stop();
+        intent = new Intent(GameActivity.this, LoadingActivity.class);
+        startActivity(intent);
     }
 }
